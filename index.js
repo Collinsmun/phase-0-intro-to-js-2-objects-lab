8d1df2e12919d45f3bb7561e0f80f2c8d90f2052
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-05 16:32:41 +0300

    commit tree 1c9dbacd8d8e5cfc4cd794c9c709fb1de58ea2c9

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name:"Jack",
+    streetAddress:"Waiyaki Way"
+};                                                               
+
+//modifying non-destructively
+function updateEmployeeWithKeyAndValue(employees,key,value){
+const employeeName = {...employee};
+employeeName[key]=value;
+return employeeName;
+};
+
+
+// const employeeUpdate1 = destructivelyDeleteFromEmployeeByKey(employees,"name","Josh");
+
+// destructively modify
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+    };
+    
+    // const employeeUpdate2 = destructivelyUpdateEmployeeWithKeyAndValue(employees,"name","Steve");
+    
+    // delete non-destructively
+    function deleteFromEmployeeByKey(employee, key){
+    const delEmplee = {...employee};
+    delete delEmplee[key];
+    return delEmplee;
+    
+    };
+    
+    // const delEmplee = deleteFromEmployeeByKey(del.streetAddress);
+    
+    // destructively delete
+    function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+    };
+    
+    // const delEmplee2 = destructivelyDeleteFromEmployeeByKey(employees.streetAddress);
+    
